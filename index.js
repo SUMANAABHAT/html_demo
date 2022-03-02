97cfc216cfab56037e4dd98dcaee707ab8890107
sumanab <sumanaabhat@gmail.com>
2022-03-01 21:09:47 -0600

    modified

--- index.js
+++ index.js
@@ -8,16 +8,20 @@ buttons.map((button) => {
             case 'C':
                 display.innerText = '';
                 break;
-                case '←':
-                    if(display.innerText){
-                   display.innerText = display.innerText.slice(0, -1);}
-                    break;
-                    case '=':
-                        try{
-                        display.innerText = eval(display.innerText);
-                    } catch{
-                        display.innerText = 'Error!';}
-                        break;
+
+            case '←':
+                if(display.innerText){
+                display.innerText = display.innerText.slice(0, -1);}
+                break;
+
+            case '=':
+                try{
+                display.innerText = eval(display.innerText);}
+
+            catch{
+                display.innerText = 'Error!';}
+                break;
+                
             default:
                 display.innerText += e.target.innerText;
         }
